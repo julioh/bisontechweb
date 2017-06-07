bf6df8d13a4ef8c6867b94948ab46482be04eccb
Julio Hernandez <juliochg@gmail.com>
2017-06-07 12:51:40 -0400

    Agregando Todo el Repo

---
+++ index.php
@@ -0,0 +1,330 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<link rel="alternate" media="screen" type="application/rss+xml" href="#"/>
+<title> Bisontechsoluciones C.A. </title>
+<meta name="title" content=" Bisontechsoluciones C.A."/>
+<meta name="author" content="Luis Rivero" />
+<meta name="keywords" CONTENT="ingenieria, software, hardware, equipos, telecomunicaciones, comunicaciones, redes, linux, gnu, windows, programas, soluciones, tencologia, empresas, aplicaciones"/>
+<meta name="description" content="Pagina de Bisontechsoluciones C.A."/><link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	
+	<!--[if IE 6]>
+	<link rel="stylesheet" href="css/ie.css" type="text/css" media="all" />
+	<![endif]-->
+	
+	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+	
+	<script type="text/javascript" src="js/jquery.jcarousel.js"></script>
+	
+	<script type="text/javascript" src="js/cufon-yui.js"></script>
+	
+	<script type="text/javascript" src="js/MyriadPro.font.js"></script>
+	
+	
+	<script type="text/javascript" src="js/jquery-func.js"></script>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+	<div class="shell">
+		<!-- Logo -->
+		<h1 id="logo">
+			<a href="#">
+				<img src="logo.png" alt="Bisontech" width="180"/>
+			</a>
+		</h1>
+		<!-- /Logo -->
+	</div>
+</div>
+<!-- /Header -->
+
+<!-- Navigation -->
+<div id="navigation">
+	<div class="shell">
+		
+		<!-- Main Navigation -->
+		<ul>
+		    <li class="first"><a href="#">Principal</a></li>
+		    <li><a href="#">Sobre Bisontech</a></li>
+		    <li><a href="#">Nuestros Servicios</a></li>
+		    <li><a href="#">Portafolio</a></li>
+		    <li><a href="#">Cont&aacute;ctanos</a></li>
+		</ul>
+		<!-- /Main Navigation -->		
+	</div>
+</div>
+<!-- /Navigation -->
+
+<!-- Slider -->
+<div id="slider">
+	<div class="shell">
+		<!-- Socials -->
+		<div class="socials">
+			<div class="socials-bg">
+				<a href="#" class="link-twitter">Twitter</a>
+				<a href="#" class="link-linkedin">LinkedIn</a>
+				<a href="#" class="link-facebook">Facebook</a>
+				<a href="#" class="link-flickr">flickr</a>
+			</div>
+		</div>
+		<!-- /Socials -->
+		
+		<!-- Slider Navigation -->
+		<div class="slider-navigation">
+			<div class="slider-navigation-bg">
+				<ul>
+				    <li><a href="#">1</a></li>
+				    <li><a href="#">2</a></li>
+				    <li><a href="#">3</a></li>
+				    <li><a href="#">4</a></li>
+				    <li><a href="#">5</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- /Slider Navigation -->
+		
+		<!-- Slider Shadow -->
+		<div class="slider-shadow">&nbsp;</div>
+		<!-- /Slider Shadow -->
+		
+		<!-- Slider Holder -->
+		<div class="slider-holder">
+			<ul>
+				<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/mantenimientoredesinalambricas.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Telecomunicaciones</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Proveemos servicio de mantenimiento, instalaci&oacute;n y administraci&oacute;n de redes cableadas e inal&aacute;mbricas de alto rendimiento, certificadas con est&aacute;ndares de calidad.</p>
+						
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s...</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->
+				<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/divulgacion.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Divulgaci&oacute;n Tecnol&oacute;gica</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Creemos en que el proceso de difusi&oacute;n de informaci&oacute;n tecnol&oacute;gica por medio de investigaciones y publicaciones realizadas por nuestros equipo especializado ayudar&aacute; a desarrollar nuevas y mejores pr&aacute;cticas en el &aacute;rea tecnol&oacute;gica para aportar al desarrollo social.</p>
+				
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s...</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->
+				<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/interconexion.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Interconexion</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Aportamos a la construcci&oacute;n de conocimiento usando dispositivos de interconexi&oacute;n para as&iacute; cubrir necesidades de la sociedad en diversos &aacute;mbitos.</p>
+							
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s...</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->
+				<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/desarrollo.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Desarrollo de Software</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tenemos a su disposici&oacute;n un equipo de profesionales expertos en el &aacute;rea de desarrollo de software para brindarle la comodidad de adaptar una soluci&oacute;n empresarial a su negocio, y no adaptar su negocio a una soluci&oacute;n empresarial.</p>
+							
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s...</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->
+				<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/hardware.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Equipos Tecnol&oacute;gicos</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ofrecemos una variedad de equipos tecnol&oacute;gicos orientados a satisfacer sus necesidades.</p>
+						
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->
+<!-- Slide -->
+			    <li>
+			    	<div class="slide-image">
+			    		<img src="css/images/soporte.jpg" alt="" />
+			    	</div>
+			    	
+			    	<div class="slide-data">
+			    		<div class="slide-data-bg">
+				    		<h3>Soporte T&eacute;cnico</h3>
+				    		<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Brindamos soporte y apoyo tecnol&oacute;gico a sitio y a distancia. Mantenimiento correctivo, preventivo y proactivo a equipos de computaci&oacute;n entre otros.</p>
+						
+				    	
+				    		<a href="#" class="more"><em>M&aacute;s</em></a>
+				    	</div>
+			    	</div>
+			    </li>
+			    <!-- /Slide -->			    
+			    
+			</ul>
+		</div>
+		<!-- /Slider Holder -->
+		<!-- Socials -->
+		<div class="socials">
+			<div class="socials-bg">
+				<a href="#" class="link-twitter">Twitter</a>
+				<a href="#" class="link-linkedin">LinkedIn</a>
+				<a href="#" class="link-facebook">Facebook</a>
+				<a href="#" class="link-flickr">flickr</a>
+			</div>
+		</div>
+		<!-- /Socials -->
+	</div>
+</div>
+<!-- /Slider -->
+
+<!-- Main -->
+<div id="main">
+	<div class="shell">
+		<!-- Box - Welcome -->
+		<div class="box">
+			<h2>Bisontech?</h2>
+			
+			<div class="entry">			
+				<div class="box-image">
+					
+				</div>
+				
+				<p><strong>Hemos n&aacute;cido para usted...</strong></p>
+				<p align="justify">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Somos una compa&ntilde;ia que ofrece bienes y servicios tecnol&oacute;gicos de calidad garantiz&aacute;dos para usted y los suyos. Bisontech se especializa en el &aacute;rea de desarrollo de soluciones bajo ambiente web, telecomunicaciones, soporte t&eacute;cnico y capacitaci&oacute;n tecnologica.</p>
+			</div>
+			
+			<a href="#" class="more"><em>M&aacute;s...</em></a>
+		</div>
+		<!-- /Box -->
+		
+		<!-- Box - News -->
+		<div class="box">
+			<h2>Soluciones</h2>
+			
+			<div class="entry">
+				<div class="news">
+					<ul>
+						<!-- New - Post -->
+					    <li>
+					    	<div class="new-image">
+					    		<a href="#"><img src="css/images/academica.jpg" alt="" /></a>
+					    	</div>
+					    	
+					    	<div class="new-data">
+					    		<h5><a href="#">Soluciones Acad&eacute;micas</a></h5>
+					    		
+					    		<p align="justify">Ofrecemos soluciones acad&eacute;micas adaptadas a las necesidades de todas las organizaciones con fines educativos.</p>
+					    	</div>
+                            <div class="cl">&nbsp;</div>
+					    </li>
+						<!-- /New - Post -->
+						
+						<!-- New - Post -->
+					    <li class="last">
+					    	<div class="new-image">
+					    		<a href="#"><img src="css/images/news-2.jpg" alt="" /></a>
+					    	</div>
+					    	
+					    	<div class="new-data">
+					    		<h5><a href="#">Medicalapp</a></h5>
+					    		
+					    		<p>Aplicaciones para ayudar a los profesionales de la medicina a organizar sus historias m&eacute;dicas en linea.</p>
+					    	</div>                           
+                            <div class="cl">&nbsp;</div>
+					    </li>
+						<!-- /New - Post -->
+					</ul>
+				</div>
+			</div>
+			
+			
+		</div>
+		<!-- /Box -->
+		
+		<!-- Box - Services -->
+		<div class="box last-box">
+			<h2>Servicios</h2>
+			
+			<div class="entry">
+				<div class="services">
+					<ul>
+					    <li><a href="#">Telecomunicaciones</a></li>
+						<li><a href="#">Desarrollo de software</a></li>
+						<li><a href="#">Certificaciones</a></li>
+						<li><a href="#">Soporte T&eacute;cnico</a></li>
+						<li><a href="#">Asesoria Tecnol&oacute;gica</a></li>
+						<li><a href="#">I+D en Tecnolog&iacute;as de informaci&oacute;n</a></li>
+						<li><a href="#">Capacitaci&oacute;n</a></li>
+						<li><a href="#">Compra y Venta de Equipos</a></li>
+					</ul>
+				</div>
+			</div>
+			
+			<a href="#" class="more"><em>M&aacute;s</em></a>
+		</div>
+		<!-- /Box -->
+        <div class="cl">&nbsp;</div>
+	</div>
+</div>
+<!-- /Main -->
+
+<!-- Footer -->
+<div id="footer">
+	<div class="shell">
+		<!-- Mini Navigation -->
+		<ul>
+		    <li class="first"><a href="#">Principal</a></li>
+		    <li><a href="#">Bisontech</a></li>
+		    <li><a href="#">Nuestros Servicios</a></li>
+		    <li><a href="#">Portafolio</a></li>
+		    <li><a href="#">Cont&aacute;ctanos</a></li>
+		</ul>
+		<!-- /Mini Navigation -->
+		
+		<!-- Copyrights -->
+		<p class="copy">Bisontech 2011</p>
+		<!-- /Copyrights -->
+		
+        <div class="cl">&nbsp;</div>
+	</div>
+</div>
+<!-- /Footer -->
+
+<script type="text/javascript">htmlLoaded();</script>
+
+</body>
+</html>
